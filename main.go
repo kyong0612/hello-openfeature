44ab47f5d985b7153ae2412fb021ab9f99812162
kimkiyong <fenderst0612@gmail.com>
2023-12-23 16:57:54 +0900

    feat: init gin

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const defaultMessage = "Hello!"
+
 func main() {
-	println("Hello, World!")
+	// Initialize Go Gin
+	engine := gin.Default()
+
+	// Setup a simple endpoint
+	engine.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, defaultMessage)
+		return
+
+	})
+
+	engine.Run()
 }
